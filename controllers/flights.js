2cc6639ae102dfbe5bd08dbd194f54aaa89bc65a
Chris Mems <christopher.mems@.com>
2022-09-24 17:31:16 -0500

    add functionality to create function

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,9 +1,25 @@
+import { Flight } from "../models/flight.js"
+
+// import (Flight)
+
 function newFlight(req, res) {
   res.render('flights/new', {
     title: 'Add Flight'
   })
 }
 
+function create(req, res){
+  Flight.create(req.body)
+  .then(flight => {
+    res.redirect(`/flights/new`)
+  })
+  .catch(err => {
+    res.redirect('/flights/new')
+  })
+}
+
+
 export {
   newFlight as new,
+  create,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -7,6 +7,8 @@ import * as flightsCtrl from '../controllers/flights.js'
 /* GET users listing. */
 router.get('/new', flightsCtrl.new)
 
+router.post('/', flightsCtrl.create)
+
 export {
   router
 }
