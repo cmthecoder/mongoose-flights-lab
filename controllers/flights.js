3a8e9b80448e62f58d6f694471ab0d374e06b116
Chris Mems <christopher.mems@.com>
2022-09-25 08:34:55 -0500

    fix redirect in create function

--- controllers/flights.js
+++ controllers/flights.js
@@ -11,15 +11,31 @@ function newFlight(req, res) {
 function create(req, res){
   Flight.create(req.body)
   .then(flight => {
-    res.redirect(`/flights/new`)
+    console.log(flight)
+    res.redirect('/flights/new')
   })
   .catch(err => {
+    console.log(err)
     res.redirect('/flights/new')
   })
 }
 
+function index(req, res){
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+      flights: flights,
+      title: "All Flights"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights/new')
+  })
+}
 
 export {
   newFlight as new,
   create,
+  index,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
-
 const router = Router()
-
 import * as flightsCtrl from '../controllers/flights.js'
 
-/* GET users listing. */
+/* GET flights listing. */
+router.get('/', flightsCtrl.index) 
+
 router.get('/new', flightsCtrl.new)
 
 router.post('/', flightsCtrl.create)
